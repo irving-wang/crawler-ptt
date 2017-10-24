e765772f70ee8bbf2046c35fac4f09af1a79bfcf
Irving Wang <irving731@outlook.com>
2017-10-25 01:23:54 +0800

    fetch ppt stock

---
+++ src/main/java/com/asuscloud/mymaster/crawler/ptt/DocWebCrawler.java
@@ -0,0 +1,122 @@
+/**
+ * 
+ */
+package com.asuscloud.mymaster.crawler.ptt;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+import edu.uci.ics.crawler4j.crawler.Page;
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+import edu.uci.ics.crawler4j.parser.HtmlParseData;
+import edu.uci.ics.crawler4j.url.WebURL;
+
+/**
+ * @author Irving
+ *
+ */
+public class DocWebCrawler extends WebCrawler {
+    private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|gif|jpg"
+	    						   + "|png|mp3|mp4|zip|gz))$");
+    
+    private static Path storageFolder;
+//    private static String[] crawlDomains;
+
+    public static void configure(String storageFolderName) {
+//        crawlDomains = domain;
+
+        storageFolder = Paths.get(storageFolderName);
+        
+        if (!Files.exists(storageFolder)) {
+            try {
+		Files.createDirectories(storageFolder);
+	    } catch (IOException e) {
+		e.printStackTrace();
+	    }
+        }
+        
+//	for (String crawlDomain : crawlDomains) {
+//	    Path domainPath = storageFolder.resolve(Paths.get(crawlDomain));
+//	    System.out.println(domainPath);
+//	    if (!Files.exists(domainPath)) {
+//	            try {
+//			Files.createDirectories(domainPath);
+//		    } catch (IOException e) {
+//			e.printStackTrace();
+//		    }
+//	        }
+//	}
+    }
+
+    /**
+     * This method receives two parameters. The first parameter is the page in
+     * which we have discovered this new url and the second parameter is the new
+     * url. You should implement this function to specify whether the given url
+     * should be crawled or not (based on your crawling logic). In this example,
+     * we are instructing the crawler to ignore urls that have css, js, git, ...
+     * extensions and to only accept urls that start with
+     * "http://www.ics.uci.edu/". In this case, we didn't need the referringPage
+     * parameter to make the decision.
+     */
+    @Override
+    public boolean shouldVisit(Page referringPage, WebURL url) {
+	String href = url.getURL();
+	
+	return !FILTERS.matcher(href).matches()
+		&& !url.getPath().equals("/bbs/Stock/index1.html")
+		&& (
+//			url.getDomain().equals("ptt.cc") ||
+			href.startsWith("https://www.ptt.cc/bbs/Stock") 
+			|| href.startsWith("http://www.ptt.cc/bbs/Stock")
+			);
+    }
+
+    /**
+     * This function is called when a page is fetched and ready to be processed
+     * by your program.
+     */
+    @Override
+    public void visit(Page page) {
+	String url = page.getWebURL().getURL();
+	System.out.println("URL: " + url);
+
+	if (page.getParseData() instanceof HtmlParseData) {
+	    HtmlParseData htmlParseData = (HtmlParseData) page.getParseData();
+	    String text = htmlParseData.getText();
+	    String html = htmlParseData.getHtml();
+	    Set<WebURL> links = htmlParseData.getOutgoingUrls();
+
+	    System.out.println("Text length: " + text.length());
+	    System.out.println("Html length: " + html.length());
+	    System.out.println("Number of outgoing links: " + links.size());
+	    
+	    System.out.println(ToStringBuilder.reflectionToString(page.getWebURL()));
+
+	    WebURL webUrl = page.getWebURL();
+//	    Path htmlPath = storageFolder.resolve(page.getWebURL().getDomain()).resolve(page.getWebURL().getDocid()+".html");
+	    Path htmlPath = storageFolder.resolve(webUrl.getSubDomain() + "." + webUrl.getDomain());
+	    htmlPath = Paths.get(htmlPath.toString(), page.getWebURL().getPath());
+	    System.out.println("htmlPath="+htmlPath);
+	    
+	    try {
+		Files.createDirectories(htmlPath.getParent());
+		
+		Files.write(htmlPath, html.getBytes("UTF-8"), StandardOpenOption.CREATE,StandardOpenOption.WRITE);
+	    } catch (UnsupportedEncodingException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    } catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	    }
+	}
+    }
+}
--- src/main/java/com/asuscloud/mymaster/crawler/ptt/PttCrawlerApp.java
+++ src/main/java/com/asuscloud/mymaster/crawler/ptt/PttCrawlerApp.java
@@ -12,34 +12,35 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
  */
 public class PttCrawlerApp 
 {
-	public static void main(String[] args) throws Exception {
-        String crawlStorageFolder = "/data/crawl/root";
-        int numberOfCrawlers = 7;
+    public static void main(String[] args) throws Exception {
+	String crawlStorageFolder = "/data/crawl/root";
+	int numberOfCrawlers = 7;
 
-        CrawlConfig config = new CrawlConfig();
-        config.setCrawlStorageFolder(crawlStorageFolder);
+	CrawlConfig config = new CrawlConfig();
+	config.setCrawlStorageFolder(crawlStorageFolder);
 
-        /*
-         * Instantiate the controller for this crawl.
-         */
-        PageFetcher pageFetcher = new PageFetcher(config);
-        RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
-        RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
-        CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
+	/*
+	 * Instantiate the controller for this crawl.
+	 */
+	PageFetcher pageFetcher = new PageFetcher(config);
+	RobotstxtConfig robotstxtConfig = new RobotstxtConfig();
+	RobotstxtServer robotstxtServer = new RobotstxtServer(robotstxtConfig, pageFetcher);
+	CrawlController controller = new CrawlController(config, pageFetcher, robotstxtServer);
 
-        /*
-         * For each crawl, you need to add some seed urls. These are the first
-         * URLs that are fetched and then the crawler starts following links
-         * which are found in these pages
-         */
-        controller.addSeed("http://www.ics.uci.edu/~lopes/");
-        controller.addSeed("http://www.ics.uci.edu/~welling/");
-    	controller.addSeed("http://www.ics.uci.edu/");
+	/*
+	 * For each crawl, you need to add some seed urls. These are the first
+	 * URLs that are fetched and then the crawler starts following links
+	 * which are found in these pages
+	 */
+//	controller.addSeed("http://www.ics.uci.edu/~lopes/");
+//	controller.addSeed("http://www.ics.uci.edu/~welling/");
+	controller.addSeed("https://www.ptt.cc/bbs/Stock/");
 
-        /*
-         * Start the crawl. This is a blocking operation, meaning that your code
-         * will reach the line after this only when crawling is finished.
-         */
-        controller.start(MyCrawler.class, numberOfCrawlers);
+	DocWebCrawler.configure(crawlStorageFolder);
+	/*
+	 * Start the crawl. This is a blocking operation, meaning that your code
+	 * will reach the line after this only when crawling is finished.
+	 */
+	controller.start(DocWebCrawler.class, numberOfCrawlers);
     }
 }
